1a477fdf2890b2fe4d7e7d2ece4aa3e62badd54d
afifasoft <abdul.pesit@gmail.com>
2020-03-12 12:32:39 +0530

    first commit

---
+++ src/main/java/com/alakesoftware/springdemoannotations/SpringDemoAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.alakesoftware.springdemoannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoAnnotationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alakesoftware/springdemoannotations/SpringDemoAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alakesoftware.springdemoannotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
