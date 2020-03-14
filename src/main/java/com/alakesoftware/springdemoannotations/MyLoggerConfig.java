535d648f2a29d31ec29aef0fb20a66eddfe36b94
afifasoft <abdul.pesit@gmail.com>
2020-03-14 10:48:32 +0530

    Add Logging Messages

--- src/main/java/com/alakesoftware/springdemoannotations/MyLoggerConfig.java
+++ src/main/java/com/alakesoftware/springdemoannotations/MyLoggerConfig.java
@@ -1,10 +1,15 @@
 package com.alakesoftware.springdemoannotations;
 
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -20,9 +25,23 @@ public class MyLoggerConfig {
 	
 	@PostConstruct
 	public void initLogger() {
-		
+		System.out.println("inside initLogger()");
+		System.out.println(rootLoggerLevel);
+		System.out.println(printedLoggerLevel);
 		Level rootLevel = Level.parse(rootLoggerLevel);
 		Level printedLevel = Level.parse(printedLoggerLevel);
+		
+		Logger applicationContextLogger = Logger.getLogger(AnnotationConfigApplicationContext.class.getName());
+		
+		Logger loggerParent = applicationContextLogger.getParent();
+		
+		loggerParent.setLevel(rootLevel);
+		
+		ConsoleHandler consoleHandler = new ConsoleHandler();
+		consoleHandler.setLevel(printedLevel);
+		consoleHandler.setFormatter(new SimpleFormatter());
+		
+		loggerParent.addHandler(consoleHandler);
 		
 	}
 
