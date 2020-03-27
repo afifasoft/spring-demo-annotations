c0f144a1347eb5a4c97eeab1fdeda93940019a6c
afifasoft <abdul.pesit@gmail.com>
2020-03-27 19:36:49 +0530

    Add Pre-processing code with @initBinder

--- src/main/java/com/alakesoftware/springdemoannotations/SpringDemoAnnotationsApplication.java
+++ src/main/java/com/alakesoftware/springdemoannotations/SpringDemoAnnotationsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.alakesoftware.springdemoannotations.service.Coach;
 import com.alakesoftware.springdemoannotations.service.impl.SwimCoach;
 
-// 140
+// 151
 @SpringBootApplication
 public class SpringDemoAnnotationsApplication {
 
