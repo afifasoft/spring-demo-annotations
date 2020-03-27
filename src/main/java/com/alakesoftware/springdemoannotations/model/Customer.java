561d4aaa1a0ffe07f1eb1e1103f2035fbb405433
afifasoft <abdul.pesit@gmail.com>
2020-03-27 20:29:17 +0530

    Applying Regular Expressions

--- src/main/java/com/alakesoftware/springdemoannotations/model/Customer.java
+++ src/main/java/com/alakesoftware/springdemoannotations/model/Customer.java
@@ -1,6 +1,9 @@
 package com.alakesoftware.springdemoannotations.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -11,6 +14,13 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 
+	@Min(value = 0, message="must be greater than or equal to 0")
+	@Max(value = 10, message="must be less than or equal to 10")
+	private int freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -26,5 +36,23 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
 
 }
